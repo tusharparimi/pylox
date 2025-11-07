44db9f1712e084cf6fefc0b6659afe096db97010
tusharparimi <tushar.parimi@gmail.com>
2025-11-07 21:33:16 +0530

    comments

--- pylox/lox_callable.py
+++ pylox/lox_callable.py
@@ -15,5 +15,5 @@ class LoxCallable(ABC):
 
 class Clock(LoxCallable):
     def arity(self) -> int: return 0
-    def call(self, interpreter: Interpreter, arguments: list[object]) -> object: return time.time() # time.time() returns current timestamp in 
+    def call(self, interpreter: Interpreter, arguments: list[object]) -> object: return time.time() # time.time() returns current unix timestamp in seconds
     def __str__(self) -> str: return "<native fn>"
